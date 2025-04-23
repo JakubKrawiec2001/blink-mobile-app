5725cd2670ad22bce3471fee9b127567a2d923bc
Jakub Krawiec <kubakrawieckk04@gmail.com>
2025-04-23 13:33:38 +0200

    New project

---
+++ app/(root)/contact.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ContactPage() {
+  return (
+    <View>
+      <Text>contact</Text>
+    </View>
+  );
+}
---
+++ app/(root)/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+      <Link href="/contact">View details</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "red",
+  },
+});
